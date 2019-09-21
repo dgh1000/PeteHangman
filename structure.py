abbf7dcb1cb03b39eee20029d5ace23dfcfcf319
Mike Mossey <michaelmossey@gmail.com>
2019-09-21 15:03:21 -0700

    structure

---
+++ structure.py
@@ -0,0 +1,14 @@
+
+def runGame():
+    # inistialize game related state
+    while True:
+        # run each step of game
+        # break when nessary
+    return (state needed for big stage)
+
+def main():
+    # initialize stage and state related to whole process
+    while True:
+
+        result = runGame()
+
