8fd3e8f7c5db7961fc578f1269d45a04063040b2
Mike Mossey <michaelmossey@gmail.com>
2019-09-24 12:00:29 -0700

    "global" demo

---
+++ sortOfGlobal.py
@@ -0,0 +1,11 @@
+
+def main():
+    d = {'strikes': 1, 'selected': 'antidisestabilishtariasm', 'working' : '___' }
+    doSomething(d)
+    print( d ['strikes'])
+
+def doSomething(d):
+    d['strikes'] = 'yup!'
+    d2['strikes'] = 'yup!'
+
+main()
